1c9d71791563e515e0ff77d5ea2222936528d6de
Aleksandr Paskal Marie <paskalpaskalovich@gmail.com>
2026-08-01 11:30:20 +0300

    добавили шаблоны темплатес папку

--- notes/views.py
+++ notes/views.py
@@ -1,18 +1,10 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 from .models import Note
-from django.shortcuts import get_object_or_404
 
-# Список всех заметок
 def notes_list(request):
     notes = Note.objects.all()
-    html = "<h1>Список заметок</h1>"
-    for note in notes:
-        html += f"<p><a href='/notes/{note.id}'>{note.title}</a></p>"
-    return HttpResponse(html)
+    return render(request, 'notes/index.html', {'notes': notes})
 
-# Детальная страница одной заметки
 def note_detail(request, note_id):
     note = get_object_or_404(Note, pk=note_id)
-    return HttpResponse(f"<h1>{note.title}</h1><p>{note.body}</p>")
+    return render(request, 'notes/detail.html', {'note': note})
